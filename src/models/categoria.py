7a416907397a0c2ab4a6c0d13ebd27218badceee
João Gabriel <joaogabrielmartinssousa3@gmail.com>
2026-06-15 20:59:02 -0300

    Delete src directory

--- src/models/categoria.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from src.models.usuario import Base
-
-class Categoria(Base):
-    __tablename__ = "categorias"
-
-    id_categoria = Column(Integer, primary_key=True, index=True)
-    nome_categoria = Column(String(50), nullable=False)
