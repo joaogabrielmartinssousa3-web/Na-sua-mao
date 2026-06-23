676dd1fe771ace40e5179ea97b880e43978d7dad
reisenzo11 <reisenzo2014@hotmail.com>
2026-06-23 08:29:09 -0300

    teste

--- na-sua-mao/na-sua-mao/database.py
+++ na-sua-mao/na-sua-mao/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
-
+# teste
 
 load_dotenv() 
 
